94f2531af533cf73730b98d60c9310a8dd1709db
Rustem666666 <48807640+Rustem666666@users.noreply.github.com>
2025-02-05 13:42:17 +0300

    Update Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -10,7 +10,7 @@ class Param:
     """Добавление параметра для чатов"""
     """557827405;24.05.2022 16:07:27;abcd;True;50;67;True;70;110"""
  
-    def __init__(self, chatId, checkDate, token, tracegpu, gpulim, gputemp, tracemem, memlim, memtemp):
+    def __init__(self, chatId, checkDate, token, tracegpu, gpulim, gputemp, tracemem, memlim, memtemp, warning):
         """Constructor"""
         self.chatId = chatId #ID чата в который отправляется информация
         self.checkDate = checkDate #Дата последней отметки клиента
@@ -21,8 +21,9 @@ class Param:
         self.tracemem = tracemem # Переключение отслеживания температуры памяти
         self.memtemp = memtemp # Текущая температура памяти
         self.memlim = memlim # Лимит температуры памяти для уведомления
+        self.warning = warning #если false то комп доступен, если true то посылаем уведомление что связь потеряна
     def __repr__(self):
-        return '\n<Параметр (chatId={}, checkDate={}, token={}, gpuTemp={}, memTemp={})>'.format( self.chatId, self.checkDate.strftime("%B %d, %Y"), self.token, self.gputemp, self.memtemp)
+        return '-----------Параметр-----------\nID чата={}\nПоследняя отметка={}\nТокен клиента={}\nОтслеживание GPU={}\nТемпература GPU={}\nЛимит темп. GPU={}\nОтслеживание памяти={}\nТемпература памяти={}\nЛимит темп. памяти={}\nТревога={}'.format( self.chatId, self.checkDate.strftime("%B %d, %Y"), self.token, self.tracegpu, self.gputemp, self.gpulim, self.tracemem, self.memtemp, self.memlim, self.warning)
 
 
 def ReadParamList():
@@ -36,10 +37,12 @@ def ReadParamList():
                 if not line:
                     break
                 result = [x for x in line.split(sep)]
-                par = Param(result[0], datetime.strptime(result[1], '%d.%m.%Y %H:%M:%S'), result[2], bool(result[3]), float(result[4]), float(result[5]), bool(result[6]), float(result[7]), float(result[8]))
+                par = Param(result[0], datetime.strptime(result[1], '%d.%m.%Y %H:%M:%S'), result[2], eval(result[3]), float(result[4]), float(result[5]), eval(result[6]), float(result[7]), float(result[8]), eval(result[9]))
                 Params.append(par)
-    except :
-        Log.AddToLog(f'Ошибка при чтении файла {paramFile}')
+        return True
+    except Exception as e:
+        Log.AddToLog(f'Ошибка при чтении файла {paramFile} {e}')
+        return False
     
 def CheckParamList():
     sep = ';'
@@ -52,7 +55,7 @@ def CheckParamList():
                 if not line:
                     break
                 result = [x for x in line.split(sep)]
-                par = Param(result[0], datetime.strptime(result[1], '%d.%m.%Y %H:%M:%S'), result[2], bool(result[3]), float(result[4]), float(result[5]), bool(result[6]), float(result[7]), float(result[8]))
+                par = Param(result[0], datetime.strptime(result[1], '%d.%m.%Y %H:%M:%S'), result[2], eval(result[3]), float(result[4]), float(result[5]), eval(result[6]), float(result[7]), float(result[8]), eval(result[9]))
                 return True
     except :
         return False
@@ -61,7 +64,7 @@ def CheckParamList():
 def SaveParamList():
     with open(paramFile, "w") as file:
         for param in Params:
-           file.write(f'{param.chatId};{param.checkDate.strftime("%d.%m.%Y %H:%M:%S")};{param.token};{param.tracegpu};{param.gpulim};{param.gputemp};{param.tracemem};{param.memlim};{param.memtemp}\n')
+           file.write(f'{param.chatId};{param.checkDate.strftime("%d.%m.%Y %H:%M:%S")};{param.token};{param.tracegpu};{param.gpulim};{param.gputemp};{param.tracemem};{param.memlim};{param.memtemp};{param.warning}\n')
  
 def BackupParamList():
     if (CheckParamList()):
@@ -70,6 +73,13 @@ def BackupParamList():
         except :
             Log.AddToLog(f'Ошибка при резервировании файла {paramFile}.')
         Log.AddToLog(f'Создаём резервную копию файла {paramFile}.')
+        
+def RecoverParam():
+    try:
+        shutil.copyfile(paramFileBackup, paramFile)
+        Log.AddToLog(f'Файл {paramFile} восстановлен из резерва.')
+    except :
+        Log.AddToLog(f'Ошибка при восстановлении {paramFile}.')
 
 def ParamExist(chatidOrtoken):
     """Возвращает true если существует элемент с chatid или token"""
