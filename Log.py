42770d8a7938a89e271d2e8e4ff44a3068f6d6bb
Rustem666666 <48807640+Rustem666666@users.noreply.github.com>
2025-02-05 13:41:45 +0300

    Update Log.py

--- Log.py
+++ Log.py
@@ -2,25 +2,26 @@ import os
 from datetime import datetime
 
 logFile = 'data/log.txt'
+dir = 'data/'
 
 def AddToLog(text):
-    CheckSize(logFile)
+    
     with open(logFile, "a") as file:
         file.write(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}\n')
-        print(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}\n')
+        print(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}')
  
 def AddToChatLog(chatid , text):
-    CheckSize(chatid + '.txt')
-    with open(chatid + '.txt', "a") as file:
+    CheckSize(dir + str(chatid) + '.txt')
+    with open(dir + str(chatid) + '.txt', "a") as file:
         file.write(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}\n')
-        print(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}\n')
+        print(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: {text}')
 
 def CheckSize(file):
     try:
         if os.path.exists(file):
             file_info = os.stat(file)
-        if (file_info.st_size > 5000000): #удаляем файл если он больше 5 мб
-            os.remove(file)
-            AddToLog(f'{file} превысил допустимый размер. Удаление.')
+            if (file_info.st_size > 5000000): #удаляем файл если он больше 5 мб
+                os.remove(file)
+                AddToLog(f'{file} превысил допустимый размер. Удаление.')
     except :
         AddToLog(f'Ошибка при попытке удаления файла {file}')
